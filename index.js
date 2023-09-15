3da96146f7d6b281b0d5588feb9855e42790d77e
josueJURE <josue.jure@gmail.com>
2023-09-15 10:40:13 +0100

    feat(JS) create flipCard()

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const cards = Array.from(document.querySelectorAll(".memory-card"));
 cards.forEach((card) => {
   card.addEventListener("click", flipCard);
 });
+
+function flipCard() {
+    console.log("card flipped")
+}
