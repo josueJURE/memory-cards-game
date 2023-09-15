12239db80cbcd002cc0fe9db00a9e58dbd7dbc8f
josueJURE <josue.jure@gmail.com>
2023-09-15 10:42:00 +0100

    feat(JS) initialize firstCard, secondCard and isFlipped variables

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const cards = Array.from(document.querySelectorAll(".memory-card"));
 
+
+let firstCard, secondCard;
+let isFlipped;
+
 cards.forEach((card) => {
   card.addEventListener("click", flipCard);
 });
